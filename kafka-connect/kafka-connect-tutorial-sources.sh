2d9f42487507da05b8a761dc170da588c4562e57
jessieharada6 <jessie.harada@yahoo.com>
2022-08-18 18:23:09 +1000

    standalone source connector

--- kafka-connect/kafka-connect-tutorial-sources.sh
+++ kafka-connect/kafka-connect-tutorial-sources.sh
@@ -4,38 +4,52 @@
 # if you are using docker Toolbox
 
 # 1) Source connectors
-# Start our kafka cluster
+# use below to build the image
+git clone https://github.com/faberchri/fast-data-dev.git
+cd fast-data-dev
+docker build -t faberchri/fast-data-dev .
+# docker run --rm -p 3030:3030 faberchri/fast-data-dev
+# update the kafka-cluster image to faberchri/fast-data-dev at docker-compose
+# start our kafka cluster
 docker-compose up kafka-cluster
-# Wait 2 minutes for the kafka cluster to be started
 
 ###############
 # A) FileStreamSourceConnector in standalone mode
-# Look at the source/demo-1/worker.properties file and edit bootstrap
-# Look at the source/demo-1/file-stream-demo.properties file
-# Look at the demo-file.txt file
+# At source/connect-source-standalone
 
 # We start a hosted tools, mapped on our code
-# Linux / Mac
-docker run --rm -it -v "$(pwd)":/tutorial --net=host landoop/fast-data-dev:cp3.3.0 bash
+# mount all the files to /tutorials folder
+# Linux / Mac: RUN AT the ROOT of the project
+docker run --rm -it -v "$(pwd)":/tutorial --net=host faberchri/fast-data-dev bash
+ls /tutorial 
+# should see 3 files listed, including worker.properties, file-stream-demo-standalone.properties and demo-file.txt
 # Windows Command Line:
-docker run --rm -it -v %cd%:/tutorial --net=host landoop/fast-data-dev:cp3.3.0 bash
+# docker run --rm -it -v %cd%:/tutorial --net=host landoop/fast-data-dev:cp3.3.0 bash
 # Windows Powershell:
-docker run --rm -it -v ${PWD}:/tutorial --net=host landoop/fast-data-dev:cp3.3.0 bash
+# docker run --rm -it -v ${PWD}:/tutorial --net=host landoop/fast-data-dev:cp3.3.0 bash
 
-# we launch the kafka connector in standalone mode:
-cd /tutorial/source/demo-1
 # create the topic we write to with 3 partitions
 kafka-topics --create --topic demo-1-standalone --partitions 3 --replication-factor 1 --zookeeper 127.0.0.1:2181
+# we launch the kafka connector in standalone mode:
+# create a standalone connector under this file
+cd /tutorial/source/connect-source-standalone
 # Usage is connect-standalone worker.properties connector1.properties [connector2.properties connector3.properties]
+# worker.properties must be named as such
 connect-standalone worker.properties file-stream-demo-standalone.properties
-# write some data to the demo-file.txt !
-# shut down the terminal when you're done.
+# write some data to the demo-file.txt
+# go to http://localhost:3030/kafka-topics-ui/#/cluster/fast-data-dev/topic/n/demo-1-standalone/data
+# at demo-file.txt, leave a line at the end, so can see the data
+# shut down the terminal when you're done
+# then start, the topic knows where to read from
+connect-standalone worker.properties file-stream-demo-standalone.properties
+# the old data is saved and can still see, write more data, data is loaded
 ###############
 
 ###############
 # B) FileStreamSourceConnector in distributed mode:
+# cd /tutorial/source/connect-source-distributed
 # create the topic we're going to write to
-docker run --rm -it --net=host landoop/fast-data-dev:cp3.3.0 bash
+docker run --rm -it --net=host faberchri/fast-data-dev bash
 kafka-topics --create --topic demo-2-distributed --partitions 3 --replication-factor 1 --zookeeper 127.0.0.1:2181
 # you can now close the new shell
 
@@ -52,7 +66,7 @@ echo "hello" >> demo-file.txt
 echo "from the other side" >> demo-file.txt
 
 # Read the topic data
-docker run --rm -it --net=host landoop/fast-data-dev:cp3.3.0 bash
+docker run --rm -it --net=host faberchri/fast-data-dev bash
 kafka-console-consumer --topic demo-2-distributed --from-beginning --bootstrap-server 127.0.0.1:9092
 # observe we now have json as an output, even though the input was text!
 ###############
